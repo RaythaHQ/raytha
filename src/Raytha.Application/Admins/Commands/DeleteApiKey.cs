521668bd242f7e664c1b69d2a8d05f02d1f52840
Zack Schwartz <apex19@gmail.com>
2023-01-21 10:05:24 -0600

    WIP: Api key management #9

---
+++ src/Raytha.Application/Admins/Commands/DeleteApiKey.cs
@@ -0,0 +1,36 @@
+﻿using CSharpVitamins;
+using MediatR;
+using Raytha.Application.Common.Exceptions;
+using Raytha.Application.Common.Interfaces;
+using Raytha.Application.Common.Models;
+
+namespace Raytha.Application.Admins.Commands;
+
+public class DeleteApiKey
+{
+    public record Command : LoggableEntityRequest<CommandResponseDto<ShortGuid>>
+    {
+    }
+
+    public class Handler : IRequestHandler<Command, CommandResponseDto<ShortGuid>>
+    {
+        private readonly IRaythaDbContext _db;
+        public Handler(IRaythaDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<CommandResponseDto<ShortGuid>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var entity = _db.ApiKeys.FirstOrDefault(p => p.Id == request.Id.Guid);
+            if (entity == null)
+                throw new NotFoundException("Api Key", request.Id);
+
+            _db.ApiKeys.Remove(entity);
+
+            await _db.SaveChangesAsync(cancellationToken);
+
+            return new CommandResponseDto<ShortGuid>(request.Id);
+        }
+    }
+}
---
+++ src/Raytha.Application/Admins/Queries/GetApiKeysForAdmin.cs
@@ -0,0 +1,35 @@
+﻿using CSharpVitamins;
+using MediatR;
+using Raytha.Application.Common.Interfaces;
+using Raytha.Application.Common.Models;
+using Raytha.Application.Common.Utils;
+using Raytha.Domain.ValueObjects;
+
+namespace Raytha.Application.Admins.Queries;
+
+public class GetApiKeysForAdmin
+{
+    public record Query : GetPagedEntitiesInputDto, IRequest<IQueryResponseDto<ListResultDto<ApiKeyDto>>>
+    {
+        public ShortGuid UserId { get; init; }
+        public override string OrderBy { get; init; } = $"CreationTime {SortOrder.ASCENDING}";
+    }
+
+    public class Handler : RequestHandler<Query, IQueryResponseDto<ListResultDto<ApiKeyDto>>>
+    {
+        private readonly IRaythaDbContext _db;
+        public Handler(IRaythaDbContext db)
+        {
+            _db = db;
+        }
+        protected override IQueryResponseDto<ListResultDto<ApiKeyDto>> Handle(Query request)
+        {
+            var query = _db.ApiKeys.Where(p => p.UserId == request.UserId.Guid).AsQueryable();
+
+            var total = query.Count();
+            var items = query.ApplyPaginationInput(request).Select(ApiKeyDto.GetProjection()).ToArray();
+
+            return new QueryResponseDto<ListResultDto<ApiKeyDto>>(new ListResultDto<ApiKeyDto>(items, total));
+        }
+    }
+}
