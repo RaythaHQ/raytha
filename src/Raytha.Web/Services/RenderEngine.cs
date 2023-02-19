7891c9bcb282f81e8d3d2883b09f422b16e73145
Zack Schwartz <apex19@gmail.com>
2023-02-19 09:24:19 -0600

    Pass in current org timezone #68

--- src/Raytha.Web/Services/RenderEngine.cs
+++ src/Raytha.Web/Services/RenderEngine.cs
@@ -12,10 +12,12 @@ public class RenderEngine : IRenderEngine
 {
     private static readonly FluidParser _parser = new FluidParser(new FluidParserOptions { AllowFunctions = true });
     private readonly IRelativeUrlBuilder _relativeUrlBuilder;
+    private readonly ICurrentOrganization _currentOrganization;
 
-    public RenderEngine(IRelativeUrlBuilder relativeUrlBuilder)
+    public RenderEngine(IRelativeUrlBuilder relativeUrlBuilder, ICurrentOrganization currentOrganization)
     {
         _relativeUrlBuilder = relativeUrlBuilder;
+        _currentOrganization = currentOrganization;
     }
 
     public string RenderAsHtml(string source, object entity)
@@ -24,7 +26,7 @@ public class RenderEngine : IRenderEngine
         {
             var options = new TemplateOptions();
             options.MemberAccessStrategy = new UnsafeMemberAccessStrategy();
-            options.TimeZone = DateTimeExtensions.GetTimeZoneInfo(DateTimeExtensions.DEFAULT_TIMEZONE);
+            options.TimeZone = DateTimeExtensions.GetTimeZoneInfo(_currentOrganization.TimeZone);
             options.Filters.AddFilter("raytha_attachment_url", RaythaAttachmentUrl);
             options.Filters.AddFilter("organization_time", LocalDateFilter);
 
