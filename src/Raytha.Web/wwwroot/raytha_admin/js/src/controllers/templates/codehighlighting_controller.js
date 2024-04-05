a5ea65e94695f3cd2c2ccc9e0f590eeca1e08dca
Zack Schwartz <apex19@gmail.com>
2024-04-05 18:50:55 -0500

    updates monaco editor #186

--- src/Raytha.Web/wwwroot/raytha_admin/js/src/controllers/templates/codehighlighting_controller.js
+++ src/Raytha.Web/wwwroot/raytha_admin/js/src/controllers/templates/codehighlighting_controller.js
@@ -1,5 +1,4 @@
 import { Controller } from "stimulus"
-//import * as monaco from 'monaco-editor'
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
 export default class extends Controller {
