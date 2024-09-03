getopenwater/cmsresearch

Added themes feature.

src/Raytha.Application/Common/Interfaces/IRaythaDbContext.cs
@@ -34,6 +34,10 @@ public interface IRaythaDbContext
     public DbSet<NavigationMenu> NavigationMenus { get; }
     public DbSet<NavigationMenuRevision> NavigationMenuRevisions { get; }
     public DbSet<NavigationMenuItem> NavigationMenuItems { get; }
+    public DbSet<Theme> Themes { get; }
+    public DbSet<ThemeRevision> ThemeRevisions { get; }
+    public DbSet<ThemeAccessToMediaItem> ThemeAccessToMediaItems { get; }
+    public DbSet<ThemeWebTemplatesMapping> ThemeWebTemplatesMappings { get; }
     public DbContext DbContext { get; }
 
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);

src/Raytha.Application/Themes/Commands/EditTheme.cs
@@ -1,0 +1,72 @@
+﻿using CSharpVitamins;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Raytha.Application.Common.Exceptions;
+using Raytha.Application.Common.Interfaces;
+using Raytha.Application.Common.Models;
+
+namespace Raytha.Application.Themes.Commands;
+
+public class EditTheme
+{
+    public record Command : LoggableEntityRequest<CommandResponseDto<ShortGuid>>
+    {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+
+        public static Command Empty() => new()
+        {
+            Title= string.Empty,
+            Description = string.Empty,
+        };
+    }
+
+    public class Validator : AbstractValidator<Command>
+    {
+        public Validator(IRaythaDbContext db)
+        {
+            RuleFor(x => x.Title).NotEmpty();
+            RuleFor(x => x.Description).NotEmpty();
+            RuleFor(x => x).Custom((request, context) =>
+            {
+                if (!db.Themes.Any(rt => rt.Id == request.Id.Guid))
+                    throw new NotFoundException("Theme", request.Id);
+
+                context.AddFailure("asd");
+            });
+        }
+    }
+
+    public class Handler : IRequestHandler<Command, CommandResponseDto<ShortGuid>>
+    {
+        private readonly IRaythaDbContext _db;
+        private readonly IMediator _mediator;
+
+        public Handler(IRaythaDbContext db, IMediator mediator)
+        {
+            _db = db;
+            _mediator = mediator;
+        }
+
+        public async Task<CommandResponseDto<ShortGuid>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var theme = await _db.Themes
+                .FirstAsync(t => t.Id == request.Id.Guid, cancellationToken);
+
+            theme.Title = request.Title;
+            theme.Description = request.Description;
+
+            await _mediator.Send(new CreateThemeRevision.Command
+            {
+                ThemeId = request.Id,
+            }, cancellationToken);
+
+            _db.Themes.Update(theme);
+
+            await _db.SaveChangesAsync(cancellationToken);
+
+            return new CommandResponseDto<ShortGuid>(request.Id);
+        }
+    }
+}

src/Raytha.Application/Views/Commands/EditPublicSettings.cs
@@ -6,6 +6,7 @@ using Raytha.Application.Common.Exceptions;
 using Raytha.Application.Common.Interfaces;
 using Raytha.Application.Common.Models;
 using Raytha.Application.Common.Utils;
+using Raytha.Domain.Entities;
 
 namespace Raytha.Application.Views.Commands;
 
@@ -49,12 +50,18 @@ public class EditPublicSettings
                 if (entity == null)
                     throw new NotFoundException("View", request.Id);
 
+                var activeThemeId = db.Themes
+                    .Where(t => t.IsActive)
+                    .Select(t => t.Id)
+                    .First();
+
                 var template = db.WebTemplates
-                    .Include(p => p.TemplateAccessToModelDefinitions)
-                    .FirstOrDefault(p => p.Id == request.TemplateId.Guid);
+                    .Where(wt => wt.ThemeId == activeThemeId)
+                    .Include(wt => wt.TemplateAccessToModelDefinitions)
+                    .FirstOrDefault(wt => wt.Id == request.TemplateId.Guid);
 
                 if (template == null)
-                    throw new NotFoundException("WebTemplate", request.TemplateId);
+                    throw new NotFoundException("Template", request.TemplateId);
 
                 if (!template.TemplateAccessToModelDefinitions.Any(p => p.ContentTypeId == entity.ContentType.Id))
                 {
@@ -97,6 +104,33 @@ public class EditPublicSettings
             entity.DefaultNumberOfItemsPerPage = request.DefaultNumberOfItemsPerPage;
             entity.MaxNumberOfItemsPerPage = request.MaxNumberOfItemsPerPage;
             entity.IgnoreClientFilterAndSortQueryParams = request.IgnoreClientFilterAndSortQueryParams;
+
+            var activeThemeId = _db.Themes
+                .Where(t => t.IsActive)
+                .Select(t => t.Id)
+                .First();
+
+            var themeWebTemplateMapping = await _db.ThemeWebTemplatesMappings
+                .Where(wtm => wtm.ThemeId == activeThemeId)
+                .Where(wtm => wtm.ViewId == entity.Id)
+                .FirstOrDefaultAsync(cancellationToken);
+
+            if (themeWebTemplateMapping == null)
+            {
+                await _db.ThemeWebTemplatesMappings.AddAsync(new ThemeWebTemplatesMapping
+                {
+                    Id = Guid.NewGuid(),
+                    ThemeId = activeThemeId,
+                    WebTemplateId = request.TemplateId.Guid,
+                    ViewId = entity.Id,
+                }, cancellationToken);
+            }
+            else
+            {
+                themeWebTemplateMapping.WebTemplateId = request.TemplateId;
+
+                _db.ThemeWebTemplatesMappings.Update(themeWebTemplateMapping);
+            }
 
             await _db.SaveChangesAsync(cancellationToken);
             return new CommandResponseDto<ShortGuid>(entity.Id);

RaythaHQ/raytha

src/Raytha.Application/Common/Interfaces/IRaythaDbContext.cs
@@ -34,6 +34,10 @@ public interface IRaythaDbContext
     public DbSet<NavigationMenu> NavigationMenus { get; }
     public DbSet<NavigationMenuRevision> NavigationMenuRevisions { get; }
     public DbSet<NavigationMenuItem> NavigationMenuItems { get; }
+    public DbSet<Theme> Themes { get; }
+    public DbSet<ThemeAccessToMediaItem> ThemeAccessToMediaItems { get; }
+    public DbSet<WebTemplateViewRelation> WebTemplateViewRelations { get; }
+    public DbSet<WebTemplateContentItemRelation> WebTemplateContentItemRelations { get; }
     public DbContext DbContext { get; }
 
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);

src/Raytha.Application/Themes/Commands/EditTheme.cs
@@ -1,0 +1,65 @@
+﻿using CSharpVitamins;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Raytha.Application.Common.Exceptions;
+using Raytha.Application.Common.Interfaces;
+using Raytha.Application.Common.Models;
+
+namespace Raytha.Application.Themes.Commands;
+
+public class EditTheme
+{
+    public record Command : LoggableEntityRequest<CommandResponseDto<ShortGuid>>
+    {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+
+        public static Command Empty() => new()
+        {
+            Title= string.Empty,
+            Description = string.Empty,
+        };
+    }
+
+    public class Validator : AbstractValidator<Command>
+    {
+        public Validator(IRaythaDbContext db)
+        {
+            RuleFor(x => x.Title).NotEmpty();
+            RuleFor(x => x.Description).NotEmpty();
+            RuleFor(x => x).Custom((request, _) =>
+            {
+                if (!db.Themes.Any(rt => rt.Id == request.Id.Guid))
+                    throw new NotFoundException("Theme", request.Id);
+            });
+        }
+    }
+
+    public class Handler : IRequestHandler<Command, CommandResponseDto<ShortGuid>>
+    {
+        private readonly IRaythaDbContext _db;
+        private readonly IMediator _mediator;
+
+        public Handler(IRaythaDbContext db, IMediator mediator)
+        {
+            _db = db;
+            _mediator = mediator;
+        }
+
+        public async Task<CommandResponseDto<ShortGuid>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var theme = await _db.Themes
+                .FirstAsync(t => t.Id == request.Id.Guid, cancellationToken);
+
+            theme.Title = request.Title;
+            theme.Description = request.Description;
+
+            _db.Themes.Update(theme);
+
+            await _db.SaveChangesAsync(cancellationToken);
+
+            return new CommandResponseDto<ShortGuid>(request.Id);
+        }
+    }
+}

src/Raytha.Application/Views/Commands/EditPublicSettings.cs
@@ -6,6 +6,7 @@ using Raytha.Application.Common.Exceptions;
 using Raytha.Application.Common.Interfaces;
 using Raytha.Application.Common.Models;
 using Raytha.Application.Common.Utils;
+using Raytha.Domain.Entities;
 
 namespace Raytha.Application.Views.Commands;
 
@@ -49,14 +50,16 @@ public class EditPublicSettings
                 if (entity == null)
                     throw new NotFoundException("View", request.Id);
 
-                var template = db.WebTemplates
-                    .Include(p => p.TemplateAccessToModelDefinitions)
-                    .FirstOrDefault(p => p.Id == request.TemplateId.Guid);
+                var templateAccessToModelDefinitions = db.WebTemplates
+                    .Include(wt => wt.TemplateAccessToModelDefinitions)
+                    .Where(wt => wt.Id == request.TemplateId.Guid)
+                    .Select(wt => wt.TemplateAccessToModelDefinitions)
+                    .FirstOrDefault();
 
-                if (template == null)
-                    throw new NotFoundException("WebTemplate", request.TemplateId);
+                if (templateAccessToModelDefinitions == null)
+                    throw new NotFoundException("Template", request.TemplateId);
 
-                if (!template.TemplateAccessToModelDefinitions.Any(p => p.ContentTypeId == entity.ContentType.Id))
+                if (!templateAccessToModelDefinitions.Any(p => p.ContentTypeId == entity.ContentType.Id))
                 {
                     context.AddFailure(Constants.VALIDATION_SUMMARY, "This template does not have access to this model definition.");
                     return;
@@ -91,12 +94,21 @@ public class EditPublicSettings
                 .Include(p => p.Route)
                 .First(p => p.Id == request.Id.Guid);
 
-            entity.WebTemplateId = request.TemplateId;
             entity.Route.Path = request.RoutePath.ToUrlSlug();
             entity.IsPublished = request.IsPublished;
             entity.DefaultNumberOfItemsPerPage = request.DefaultNumberOfItemsPerPage;
             entity.MaxNumberOfItemsPerPage = request.MaxNumberOfItemsPerPage;
             entity.IgnoreClientFilterAndSortQueryParams = request.IgnoreClientFilterAndSortQueryParams;
+
+            var activeThemeId = await _db.OrganizationSettings
+                .Select(os => os.ActiveThemeId)
+                .FirstAsync(cancellationToken);
+
+            var webTemplateViewRelation = await _db.WebTemplateViewRelations
+                .FirstAsync(wtr => wtr.ViewId == entity.Id && wtr.WebTemplate!.ThemeId == activeThemeId, cancellationToken);
+
+            webTemplateViewRelation.WebTemplateId = request.TemplateId.Guid;
+            _db.WebTemplateViewRelations.Update(webTemplateViewRelation);
 
             await _db.SaveChangesAsync(cancellationToken);
             return new CommandResponseDto<ShortGuid>(entity.Id);